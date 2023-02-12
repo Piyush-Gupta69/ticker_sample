9fe68c0ca1d184642dd76e930c46560a7f09018c
Piyush Gupta <pgtweety1234@gmail.com>
2023-02-12 15:18:23 +0530

    first commit

---
+++ ticker/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+        
+        <div class="news-container">
+            <div class="title">
+                NOTE:
+            </div>
+    
+            <ul>
+                <li>
+                Online Painting Competition has been extended till 28th February,2023. Click to know more
+                </li>
+            </ul>
+        </div>
+  );
+}
+
+export default App;
